4db7e57a81089793ca8657b3ab325f3c9a2fda15
matthewyoungjr <hawkeyeanon@protonmail.com>
2024-05-02 11:36:45 +0000

    same as last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import "./App.css";
+import Filter from "./components/Filter";
 import Form from "./components/Form";
 import Table from "./components/Table";
+import { useState } from "react";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 2000, category: "Utilities" },
+    { id: 2, description: "aab", amount: 2000, category: "Utilities" },
+    { id: 3, description: "aac", amount: 2000, category: "Utilities" },
+    { id: 4, description: "Milk", amount: 2000, category: "Utilities" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
   return (
     <>
       <Form />
-      <Table />
+      <Filter onSelectCategory={(cat) => setSelectedCategory(cat)} />
+      <Table
+        expenses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
     </>
   );
 }
